69b3b8b702370e5f130dfdc46c241f6b64347ba9
rickrribeiro <rickribeiro99@gmail.com>
2021-11-27 16:19:22 -0300

    base nova

--- bot/index.py
+++ bot/index.py
@@ -1,3 +1,17 @@
-from dotenv import dotenv_values
-config = dotenv_values(".env")
-print(config['TESTE'])
+# from dotenv import dotenv_values
+# config = dotenv_values(".env")
+# print(config['TESTE'])
+
+state=10
+def teste():
+  global state
+  print(state)
+
+def main():
+  
+  state =12
+  teste()
+
+
+if __name__ == '__main__':
+    main()
