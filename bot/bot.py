1766e20e59c3b954b2fccacda00008fe4b40263a
rickrribeiro <rickribeiro99@gmail.com>
2021-11-27 14:26:09 -0300

    limpando o arquivo do bot

--- bot/bot.py
+++ bot/bot.py
@@ -4,28 +4,11 @@
 
 from dotenv import dotenv_values
 config = dotenv_values(".env")
-
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-
 logger = logging.getLogger(__name__)
 
 
@@ -53,30 +36,20 @@ def error(update, context):
 
 def main():
     """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
     updater = Updater(config['TOKEN_TELEGRAM'], use_context=True)
-
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
-
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_error_handler(error)
 
-    # on noncommand i.e message - echo the message on Telegram
+    # Tratamento quando chega alguma mensagem
     dp.add_handler(MessageHandler(Filters.text, echo))
 
-    # log all errors
-    dp.add_error_handler(error)
 
     # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+    # Deixa o bot rodando
     updater.idle()
 
 
